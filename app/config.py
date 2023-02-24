820baffc8467085b83d6feaeaa304f46330409dd
Sabrina Graham <techiepotato3004@gmail.com>
2023-02-24 14:52:41 -0500

    minor fixes

--- app/config.py
+++ app/config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 class Config(object):
     DEBUG = False
-    SECRET_KEY = os.environ.get ('SECRET_KEY', 'Som3$ecr3tk*y')
+    SECRET_KEY = os.environ.get ('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','postgresql://sabby:thel0gger@localhost/logindata'.replace('postgres://','postgres://'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
